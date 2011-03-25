42dd1a6e354e45669b63ae4d9321274d8977dcf2
Carlos Brando <eduardobrando@gmail.com>
2011-03-25 16:59:18 -0300

    Coloca o chat para funcionar em modo multinode.

--- chat_client.erl
+++ chat_client.erl
@@ -2,16 +2,23 @@
 -compile(export_all).
 
 register_nickname (Nickname) ->
-  message_router:register_nick(Nickname, fun(Msg) -> chat_client:print_message(Nickname, Msg) end).
+  Pid = spawn(chat_client, handle_messages, [Nickname]),
+  message_router:register_nick(Nickname, Pid).
 
 unregister_nickname (Nickname) ->
   message_router:unregister_nick(Nickname).
 
-send_message (Addressee, MessageBody) ->
-  message_router:send_chat_message(Addressee, MessageBody).
-  
-print_message (Who, MessageBody) ->
-  io:format("~p received: ~p~n", [Who, MessageBody]).
+send_message (Nickname, MessageBody) ->
+  message_router:send_chat_message(Nickname, MessageBody).
+
+handle_messages (Nickname) ->
+  receive
+    {printmsg, MessageBody} ->
+      io:format("~p received: ~p~n", [Nickname, MessageBody]),
+      handle_messages(Nickname);
+    stop ->
+      ok
+  end.
 
 start_router () ->
   message_router:start().
--- message_router.erl
+++ message_router.erl
@@ -3,38 +3,58 @@
 -compile(export_all).
 
 start () ->
-  Pid = spawn(message_router, route_messages, [dict:new()]),
-  erlang:register(?SERVER, Pid).
+  global:trans({?SERVER, ?SERVER}, fun
+      () ->
+        case global:whereis_name(?SERVER) of
+          undefined ->
+            Pid = spawn(message_router, route_messages, [dict:new()]),
+            global:register_name(?SERVER, Pid);
+          _ ->
+            ok
+        end
+    end).
 
 stop () ->
-  ?SERVER ! shutdown.
+  global:trans({?SERVER, ?SERVER}, fun
+    () ->
+      case global:whereis_name(?SERVER) of
+        undefined ->
+          ok;
+        _ ->
+          global:send(?SERVER, shutdown)
+      end
+  end).
+  
+send_chat_message (ClientName, MessageBody) ->
+  global:send(?SERVER, {send_chat_msg, ClientName, MessageBody}).
 
-send_chat_message (Addressee, MessageBody) ->
-  ?SERVER ! {send_chat_msg, Addressee, MessageBody}.
-
-register_nick (ClientName, PrintFun) ->
-  ?SERVER ! {register_nick, ClientName, PrintFun}.
+register_nick (ClientName, ClientPid) ->
+  global:send(?SERVER, {register_nick, ClientName, ClientPid}).
 
 unregister_nick (ClientName) ->
-  ?SERVER ! {unregister_nick, ClientName}.
+  global:send(?SERVER, {unregister_nick, ClientName}).
 
 route_messages (Clients) ->
   receive
     {send_chat_msg, ClientName, MessageBody} ->
-      ?SERVER ! {recv_chat_msg, ClientName, MessageBody},
-      route_messages(Clients);
-    {recv_chat_msg, ClientName, MessageBody} ->
       case dict:find(ClientName, Clients) of
-        {ok, PrintFun} ->
-          PrintFun(MessageBody);
+        {ok, ClientPid} ->
+          ClientPid ! {printmsg, MessageBody};
         error ->
-          io:format("Unknown client~n")
+          io:format("Error! Unknown client: ~p~n", [ClientName])
       end,
       route_messages(Clients);
-    {register_nick, ClientName, PrintFun} ->
-      route_messages(dict:store(ClientName, PrintFun, Clients));
+    {register_nick, ClientName, ClientPid} ->
+      route_messages(dict:store(ClientName, ClientPid, Clients));
     {unregister_nick, ClientName} ->
-      route_messages(dict:erase(ClientName, Clients));
+      case dict:find(ClientName, Clients) of
+        {ok, ClientPid} ->
+          ClientPid ! stop,
+          route_messages(dict:erase(ClientName, Clients));
+        error ->
+          io:format("Error! Unknown client: ~p~n", [ClientName]),
+          route_messages(Clients)
+      end;
     shutdown ->
       io:format("Shutting down~n");
     Oops ->
